0cf618b36b24abcbee2cccf414734e8b46e87f99
Shewoot <shewoot@yahoo.com>
2017-09-09 12:04:02 -0500

    Add files via upload

--- ChallengeMultiplicationTables.java
+++ ChallengeMultiplicationTables.java
@@ -1,11 +1,9 @@
 
 /**
  * Write a description of class ChallengeMultiplicationTables here.
- *
  * @author Sheila Wooten
  * @version 8/28/17)
  */
-
 import java.util.Scanner;
 import java.io.*;
 public class ChallengeMultiplicationTables
@@ -14,33 +12,49 @@ public class ChallengeMultiplicationTables
     {
         Scanner S = new Scanner(System.in);
         System.out.println("Welcome to Multiplication Tables");
-        System.out.println("How large would you like to see the them?  ");
+        System.out.println("Enter a number between: 0 - 20 to See your Table!!  ");
         int size = S.nextInt();
+        if ( size > 20 )
+            {
+                System.out.print("Try again....Your number is not 20 or less!!");
+                return;
+            }
+            else
+            {
+                System.out.print("Great you are within parameters of 1-20!!");
+                System.out.println("  ");
+            }
+        int k = 0;
         System.out.print("  ");
+    
         for(int i = 0; i <= size; i++)
         {   
-            System.out.format("%4d", i );
+            System.out.format("%4d", i);
         }
         System.out.println();
         for(int i = 0; i <= size; i++)
         {
             if (i < 10)
-            {
-                System.out.print(" " + i);
-            }
-            else
-            {
-                System.out.print(i);
-            }
+                {
+                    System.out.print(" " + i);
+                }
+                else
+                {
+                    System.out.print(i);
+                }
             for(int j = 0; j <= size; j++)
             {
-                int k =  i*j;
-                if(k < 20)
+                k = i*j;
+                if (k < 20)
+                {
                     System.out.format("%4d", k);
+                }
                 else
+                {
                     System.out.format("%4d", k);
+                }  
             }
             System.out.println();
-        }           
+        }
     }
 }
